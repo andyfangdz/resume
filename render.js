0a08e9d2ea2e948f10d43b4a85fd84253fb83559
Andy Fang <andyfang.dz@gmail.com>
2016-09-09 13:33:26 -0400

    add nightmare log

--- render.js
+++ render.js
@@ -2,6 +2,7 @@ var Nightmare = require('nightmare');
 var nightmare = Nightmare({ show: false });
 var fs = require('fs');
 
+console.log("Starting nightmare...");
 nightmare
   .goto('http://localhost:9000/resume')
   .wait(1000)
@@ -12,6 +13,7 @@ nightmare
     return document.getElementsByTagName('html')[0].innerHTML;
   }).then(function(html) {
     fs.writeFile('build/200.html', html);
+    console.log("Nightmare built.");
     return nightmare.end();
   })
   .catch(function (error) {
