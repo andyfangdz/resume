fd46ebf63fd8f3aa9cd4e96da6be06d9d8e85bc3
Andy Fang <andyfang.dz@gmail.com>
2016-09-09 17:01:00 -0400

    add awards

--- src/data/004-awards.js
+++ src/data/004-awards.js
@@ -23,4 +23,58 @@ Dezhi “Andy” Fang received:
 `,
   date: "Jun. 2016",
   featured: true
+}, {
+  "title": "Runner Up, Emory GCC Case Competition",
+  "date": "Apr. 2016",
+  "awardedBy": "Emory Global China Connection",
+  "featured": false,
+  "notes": "Case competition focused on mobile payment industry. Developed strategy for a U.S. based company to enter China's market with NFC payment technology."
+}, {
+  "title": "Runner Up, BizTech'16 Case Competition",
+  "date": "Mar. 2016",
+  "awardedBy": "Georgia Tech Management Information Systems (MIS) Club",
+  "featured": false,
+  "notes": "Case competition focused on Electronic Health Record (EHR) industry. Developed strategy for an established EHR company to transform its infrastructure to cloud-based solutions with high interoperability among EHR standards and medical devices."
+}, {
+  "title": "Runner Up in Education Track, HackDuke'15",
+  "date": "Nov. 2015",
+  "awardedBy": "HackDuke",
+  "featured": false,
+  "notes": "Developed ResumeWorks, a platform for creating different versions of resumes to fit each job application."
+}, {
+  "title": "Hacker's Choice Award, VandyHacks'15",
+  "date": "Oct. 2015",
+  "awardedBy": "VandyHacks",
+  "featured": false,
+  "notes": "Developed Bank Guard, an app for detecting credit card frauds."
+}, {
+  "title": "4th Place, IronCoder'15",
+  "date": "Sep. 2015",
+  "awardedBy": "Cardlytics",
+  "featured": false,
+  "notes": "A semiannually held programming competition by Cardlytics."
+}, {
+  "title": "First Place, Georgia Tech College of Computing Alumni Hackathon'15",
+  "date": "Aug. 2015",
+  "awardedBy": "Georgia Tech College Of Computing",
+  "featured": false,
+  "notes": "Developed MapsOnPoint, an app for finding points of interest along your road trip route without taking an unreasonable detour."
+}, {
+  "title": "Georgia Tech Online Masters Program Scholarship",
+  "date": "Aug. 2015",
+  "awardedBy": "Georgia Tech College Of Computing",
+  "featured": false,
+  "notes": "A full scholarship of the Georgia Tech Online Masters Program (worth $7000) was awarded."
+}, {
+  "title": "Third Prize, China Adolescents Science & Technology Innovation Contest",
+  "date": "Aug. 2014",
+  "awardedBy": "China Association for Science and Technology",
+  "featured": false,
+  "notes": "Presented a stereo in-door location system using computer vision."
+}, {
+  "title": "First Prize, National Olympiad in Informatics in Provinces",
+  "date": "Dev. 2013",
+  "awardedBy": "China Computer Federation (CCF)",
+  "featured": false,
+  "notes": "Programming competition in a form similar to ACM-ICPC."
 }];
