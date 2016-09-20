0db7bd3cd807c7374f72b2b45d9c203ed68001c3
Dezhi “Andy” Fang <AndyFang.DZ@gmail.com>
2016-09-20 18:44:21 +0200

    Update 003-publications.js

--- src/data/003-publications.js
+++ src/data/003-publications.js
@@ -2,6 +2,17 @@
  * Created by andy on 9/7/16.
  */
 export default [{
+  title: "[Presenter] M-Flash: Fast Billion-scale Graph Computation Using a Bimodal Block Processing Model",
+  authors: ["Hugo Gualdron", "Robson Cordeiro", "Jose Rodrigues-Jr", "Duen Horng (Polo) Chau", "Minsuk Kahng", "U Kang"],
+  venue: "Joint European Conference on Machine Learning and Knowledge Discovery in Databases",
+  links: {
+    "Slides": "https://assets.andyfang.me/slides/16-m-flash-pkdd.pdf"
+    "arXiv": "https://arxiv.org/abs/1506.01406",
+    "PDF": "https://arxiv.org/pdf/1506.01406v5"
+  },
+  date: "Sep. 2016",
+  featured: false
+}, {
   title: "Visual Exploration of Machine Learning Results using Data Cube Analysis",
   authors: ["Minsuk Kahng", "Dezhi Fang", "Duen Horng (Polo) Chau"],
   venue: "Proceedings of the Workshop on Human-In-the-Loop Data Analytics (HILDA’16) (Co-located with SIGMOD/PODS’16)",
