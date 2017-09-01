518076a140036c9ed474a6778707aa381123cf32
Andy Fang <andyfang.dz@gmail.com>
2017-09-01 14:13:15 -0400

    fix some styles

--- src/components/CV/control/ControlBar.js
+++ src/components/CV/control/ControlBar.js
@@ -1,85 +1,101 @@
-/**
- * Created by andy on 2/28/17.
- */
-import React, {Component} from 'react';
-import {observer} from "mobx-react";
+import React, { Component } from 'react';
+import { observer } from 'mobx-react';
 import Route from 'route-parser';
 
 import { FormGroup, FormControlLabel } from 'material-ui/Form';
 import Switch from 'material-ui/Switch';
 import Checkbox from 'material-ui/Checkbox';
 import Button from 'material-ui/Button';
+import { withStyles } from 'material-ui/styles';
 import Paper from 'material-ui/Paper';
 import FileDownloadIcon from 'material-ui-icons/FileDownload';
 import PrintIcon from 'material-ui-icons/Print';
 
-import appState, {history} from "../../../store/appState";
+import appState, { history } from '../../../store/appState';
+
+const styles = theme => ({
+  button: {
+    margin: 0,
+    marginBottom: '10px',
+  },
+  input: {
+    display: 'none',
+  },
+});
 
 class ControlBar extends Component {
 
-    constructor(props) {
-        super(props);
-        this.routeParser = new Route('/:name');
+  constructor(props) {
+    super(props);
+    this.routeParser = new Route('/:name');
+  }
+
+  componentWillMount() {
+    appState.cv.featured = this.routeParser.match(window.location.pathname).name === 'resume';
+  }
+
+  onSwitchCVResume = () => {
+    if (appState.cv.featured) {
+      history.push('/cv');
+    } else {
+      history.push('/resume')
     }
+    appState.cv.featured = !appState.cv.featured;
+  };
 
-    componentWillMount() {
-        appState.cv.featured = this.routeParser.match(window.location.pathname).name === "resume";
-    }
+  onSwitchSeal = () => {
+    appState.cv.showSeal = !appState.cv.showSeal;
+  };
 
-    onSwitchCVResume = () => {
-        if (appState.cv.featured) {
-            history.push("/cv");
-        } else {
-            history.push("/resume")
-        }
-        appState.cv.featured = !appState.cv.featured;
-    };
+  render() {
+    const classes = this.props.classes;
+    return (
+      <Paper className="toolbar-paper">
+        <div>
+          <span>CV</span>
+          <Switch
+            checked={appState.cv.featured}
+            onChange={this.onSwitchCVResume}
+          />
+          <span>Resume</span>
+        </div>
+        <FormGroup row={true}>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={appState.cv.showSeal}
+                onChange={this.onSwitchSeal}
+              />
+            }
+            label="Show Seal"
+          />
+        </FormGroup>
+        <Button
+          className={classes.button}
+          raised={true}
+          color="primary"
+          onClick={() => {
+            window.open(
+              `https://s3.andyfang.me/cv/${(appState.cv.featured ? 'resume.pdf' : 'cv.pdf')}`,
+              '_blank');
+          }}
+        >
+          <FileDownloadIcon/> PDF
+        </Button>
+        <Button
+          className={classes.button}
+          raised={true}
+          color="accent"
+          onClick={() => {
+            window.print();
+          }}
+        >
+          <PrintIcon/> Print
+        </Button>
 
-    onSwitchSeal = () => {
-        appState.cv.showSeal = !appState.cv.showSeal;
-    };
-
-    render() {
-        return (<Paper className="toolbar-paper" rounded={false}>
-            CV
-            <Switch
-                checked={appState.cv.featured}
-                onChange={this.onSwitchCVResume}
-            />
-            Resume <br />
-
-            <FormGroup row>
-                <FormControlLabel
-                    control={
-                        <Checkbox
-                            checked={appState.cv.showSeal}
-                            onChange={this.onSwitchSeal}
-                        />
-                    }
-                    label="Show Seal"
-                />
-            </FormGroup>
-            <Button
-                raised={true}
-                color="primary"
-                onClick={() => {
-                    window.open(`https://s3.andyfang.me/cv/${(appState.cv.featured ? 'resume.pdf' : 'cv.pdf')}`, '_blank');
-                }}
-            >
-                <FileDownloadIcon /> PDF
-            </Button>
-            <Button
-                raised={true}
-                color="accent"
-                onClick={() => {
-                    window.print();
-                }}
-            >
-                <PrintIcon /> Print
-            </Button>
-
-        </Paper>);
-    }
+      </Paper>
+    );
+  }
 }
 
-export default observer(ControlBar);
+export default observer(withStyles(styles)(observer(ControlBar)));
