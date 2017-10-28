a1c427aaa922d7932f3163fb7a63ec34993febad
Dezhi “Andy” Fang <AndyFang.DZ@gmail.com>
2017-10-28 01:32:57 -0400

    Update 080-press.js

--- src/data/080-press.js
+++ src/data/080-press.js
@@ -2,6 +2,14 @@
  * Created by andy on 3/1/17.
  */
 export default [{
+  title: "Georgia Tech to Participate for the First Time in Supercomputing Student Cluster Competition",
+  publisher: "The Compiler (Georgia Tech College of Computing)",
+  link: "https://www.cc.gatech.edu/news/597895/georgia-tech-participate-first-time-supercomputing-student-cluster-competition",
+  date: "Oct. 2017",
+  quote: `
+> While these students are still in their undergrad, the training and education they have received in preparation for this competition expands well beyond HPC and produces an educational foundation they will continue using for years to come.
+`
+}, {
   title: "China Economy Draws More Students Back From Abroad",
   publisher: "The Wall Street Journal",
   link: "https://www.wsj.com/articles/china-economy-draws-more-students-back-from-abroad-1488364203",
