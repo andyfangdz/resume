7cccc9d2854fe0777d28a5d68d312bd4d202b96e
PrettierCI <commits@prettierci.com>
2018-02-27 00:00:35 +0000

    Sync with Prettier

--- render.js
+++ render.js
@@ -1,25 +1,26 @@
 var Nightmare = require('nightmare');
-var nightmare = Nightmare({show: false});
+var nightmare = Nightmare({ show: false });
 var fs = require('fs');
 var pdfOptions = {
-    printBackground: false
-}
+  printBackground: false,
+};
 
-console.log("Starting nightmare...");
+console.log('Starting nightmare...');
 nightmare
-    .goto('http://localhost:9000/resume')
-    .wait(1000)
-    .pdf('build/resume.pdf', pdfOptions)
-    .goto('http://localhost:9000/cv', pdfOptions)
-    .wait(1000)
-    .pdf('build/cv.pdf')
-    .evaluate(function () {
-        return document.getElementsByTagName('html')[0].innerHTML;
-    }).then(function (html) {
+  .goto('http://localhost:9000/resume')
+  .wait(1000)
+  .pdf('build/resume.pdf', pdfOptions)
+  .goto('http://localhost:9000/cv', pdfOptions)
+  .wait(1000)
+  .pdf('build/cv.pdf')
+  .evaluate(function() {
+    return document.getElementsByTagName('html')[0].innerHTML;
+  })
+  .then(function(html) {
     fs.writeFile('build/200.html', html);
-    console.log("Nightmare built.");
+    console.log('Nightmare built.');
     return nightmare.end();
-})
-    .catch(function (error) {
-        console.error('Failed:', error);
-    });
+  })
+  .catch(function(error) {
+    console.error('Failed:', error);
+  });
