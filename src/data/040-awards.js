4501795da1423d7470ee4cf32e0aefae94babdbc
Andy Fang <andyfang.dz@gmail.com>
2018-03-29 20:15:00 -0400

    add awards

--- src/data/040-awards.js
+++ src/data/040-awards.js
@@ -1,5 +1,27 @@
 export default [
   {
+    title: 'Outstanding Undergraduate Researcher Award',
+    awardedBy: 'Undergraduate Research Opportunities Program, Georgia Institute of Technology',
+    notes: `
+Annually, one student from each department in Georgia Tech is nominated
+to receive the award on the basis of involvement in long-term research projects, 
+participation in conferences, published research papers, displayed leadership within 
+the research environment, and unique contributions to the field.
+`,
+    date: 'Apr. 2018',
+    featured: true,
+  },
+  {
+    title: 'Outstanding Undergraduate Research',
+    awardedBy: 'College of Computing, Georgia Institute of Technology',
+    notes: `
+Annually, one student from College of Computing is nominated by faculty members
+to receive the award, recognizing their involvement in undergraduate research.
+`,
+date: 'Apr. 2018',
+    featured: false,
+  },
+  {
     title: 'Material, Supplies and Technology Grants (MS&T)',
     awardedBy: 'Georgia Institute of Technology',
     notes: `
@@ -7,7 +29,7 @@ Supported by MS&T grants for resesarch in Visualization and Virtual Reality
 during Summer 2017. $1,000.
 `,
     date: 'May. - Aug. 2017',
-    featured: true,
+    featured: false,
   },
   {
     title: "President's Undergraduate Research Award (PURA)",
